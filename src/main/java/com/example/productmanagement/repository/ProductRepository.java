d2e345fdc42efefbeb34e1e31918474441873c16
Skippers <doquang9801@gmail.com>
2026-09-09 11:17:24 +0700

    Delete src/main directory

--- src/main/java/com/example/productmanagement/repository/ProductRepository.java
+++
@@ -1,4 +0,0 @@
-package com.example.productmanagement.repository;
-import com.example.productmanagement.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface ProductRepository extends JpaRepository<Product,Long>{}
